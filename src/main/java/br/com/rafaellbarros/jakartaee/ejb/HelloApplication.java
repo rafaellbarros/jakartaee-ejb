c73613483671edccafd4b281ac3102d0d6114799
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2022-10-19 16:39:06 -0400

    :construction: Implementando openapi

--- src/main/java/br/com/rafaellbarros/jakartaee/ejb/HelloApplication.java
+++ src/main/java/br/com/rafaellbarros/jakartaee/ejb/HelloApplication.java
@@ -1,9 +1,26 @@
 package br.com.rafaellbarros.jakartaee.ejb;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
+@OpenAPIDefinition(info = @Info(
+        title = "JakartaEE EJb Application",
+        version = "1.0.0",
+        contact = @Contact(
+                name = "Rafael Barros",
+                email = "rafaelbarros.df@gmail.com",
+                url = "http://www.xablau.com")
+),
+        servers = {
+                @Server(url = "/example",description = "localhost")
+        }
+)
 public class HelloApplication extends Application {
 
 }
--- src/main/java/br/com/rafaellbarros/jakartaee/ejb/resource/HelloResource.java
+++ src/main/java/br/com/rafaellbarros/jakartaee/ejb/resource/HelloResource.java
@@ -1,13 +1,19 @@
 package br.com.rafaellbarros.jakartaee.ejb.resource;
 
 import br.com.rafaellbarros.jakartaee.ejb.service.XablauService;
-
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Properties;
 
 
 @Path("/hello")
@@ -17,9 +23,23 @@ public class HelloResource {
     private XablauService xablauService;
 
     @GET
-    @Produces("text/plain")
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "404",
+                            description = "We could not find the Hello message ",
+                            content = @Content(mediaType = "text/plain"))
+                    ,
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "We found the Hello message",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Properties.class)))})
+    @Operation(summary = "Outputs Hello Message",
+            description = "This method outputs Hello message")
     public Response hello() {
-        return Response.ok(getMessageFormatted()).build();
+        return Response.ok(xablauService.xablau().toUpperCase()).build();
     }
 
     private String getMessageFormatted() {
